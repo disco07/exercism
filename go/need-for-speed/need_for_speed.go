9d452c092e64924d0a99057976b994674d0f2e92
disco07 <koneenok@outlook.fr>
2022-07-04 23:14:53 +0200

    need for speed

--- go/need-for-speed/need_for_speed.go
+++ go/need-for-speed/need_for_speed.go
@@ -25,20 +25,17 @@ func NewTrack(distance int) Track {
 // Drive drives the car one time. If there is not enough battery to drive one more time,
 // the car will not move.
 func Drive(car Car) Car {
-	car = Car{speed: car.speed, batteryDrain: car.batteryDrain, distance: car.distance, battery: car.battery}
-	if car.batteryDrain < car.battery {
+	if car.batteryDrain <= car.battery {
 		car.distance += car.speed
 		car.battery -= car.batteryDrain
+		return car
 	}
 	return car
 }
 
 // CanFinish checks if a car is able to finish a certain track.
 func CanFinish(car Car, track Track) bool {
-	car = NewCar(car.speed, car.batteryDrain)
-	track = NewTrack(track.distance)
-
-	if car.battery-car.batteryDrain < track.distance {
+	if calc := car.battery - (car.batteryDrain * car.distance); calc <= track.distance {
 		return false
 	}
 	return true
