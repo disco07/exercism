9f6ab6aa3a81cdeb2ca926e9652e2cc6c3f712e4
disco07 <koneenok@outlook.fr>
2022-07-04 13:10:54 +0200

    weather finish

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,14 @@
+// Package weather provides temperatures of country.
 package weather
 
+// CurrentCondition It has condition of weather.
 var CurrentCondition string
+
+// CurrentLocation It has location of city.
 var CurrentLocation string
 
+// Forecast returns a string of current location and
+// current condition.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
