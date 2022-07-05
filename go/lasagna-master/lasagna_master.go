04f10ea567d6ff4bf82e6d9b282c6661f40f96d4
disco07 <koneenok@outlook.fr>
2022-07-05 14:17:48 +0200

    lasagna master

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,9 +1,39 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+// PreparationTime return le estimated time to cook.
+func PreparationTime(slice []string, time int) int {
+	if time == 0 {
+		time = 2
+	}
+	return len(slice) * time
+}
 
-// TODO: define the 'Quantities()' function
+// Quantities determines quantities of needle and sauce to cook lasagna.
+func Quantities(slice []string) (noodles int, sauce float64) {
+	var qNoodle int
+	var qSauce float64
+	for _, s := range slice {
+		if s == "sauce" {
+			qSauce++
+		} else if s == "noodles" {
+			qNoodle++
+		}
+	}
+	return qNoodle * 50, qSauce * 0.2
+}
 
-// TODO: define the 'AddSecretIngredient()' function
+// AddSecretIngredient returns my list of ingredient and add secret ingredient.
+func AddSecretIngredient(friendsList []string, myList []string) []string {
+	myList[len(friendsList)] = friendsList[len(friendsList)-1]
+	return myList
+}
 
-// TODO: define the 'ScaleRecipe()' function
+// ScaleRecipe return a slice of float64 of the amounts needed
+// for the desired number of portions.
+func ScaleRecipe(amounts []float64, portions int) []float64 {
+	var quantities []float64
+	for _, quantity := range amounts {
+		quantities = append(quantities, quantity*float64(portions/2))
+	}
+	return quantities
+}
