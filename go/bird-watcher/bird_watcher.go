aa91382e840ea467e4ea826f864d97e0d574a1ef
disco07 <koneenok@outlook.fr>
2022-07-05 10:17:20 +0200

    bird watcher finish

--- go/bird-watcher/bird_watcher.go
+++ go/bird-watcher/bird_watcher.go
@@ -3,17 +3,37 @@ package birdwatcher
 // TotalBirdCount return the total bird count by summing
 // the individual day's counts.
 func TotalBirdCount(birdsPerDay []int) int {
-	panic("Please implement the TotalBirdCount() function")
+	var sum int
+	for _, v := range birdsPerDay {
+		sum += v
+	}
+	return sum
 }
 
 // BirdsInWeek returns the total bird count by summing
 // only the items belonging to the given week.
 func BirdsInWeek(birdsPerDay []int, week int) int {
-	panic("Please implement the BirdsInWeek() function")
+	var sum int
+
+	if week > 1 {
+		week = 7*week - 6
+	} else {
+		week = 0
+	}
+	for i := week; i < week+7; i++ {
+		sum += birdsPerDay[i]
+	}
+
+	return sum
 }
 
 // FixBirdCountLog returns the bird counts after correcting
 // the bird counts for alternate days.
 func FixBirdCountLog(birdsPerDay []int) []int {
-	panic("Please implement the FixBirdCountLog() function")
+	for i := 0; i < len(birdsPerDay); i++ {
+		if i%2 == 0 {
+			birdsPerDay[i] = birdsPerDay[i] + 1
+		}
+	}
+	return birdsPerDay
 }
