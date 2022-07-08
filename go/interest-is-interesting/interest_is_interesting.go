4c2d85b9210599e25f5d3a4f7530ad8f38f49d3f
disco07 <koneenok@outlook.fr>
2022-07-08 21:45:42 +0200

    interest finish

--- go/interest-is-interesting/interest_is_interesting.go
+++ go/interest-is-interesting/interest_is_interesting.go
@@ -2,20 +2,38 @@ package interest
 
 // InterestRate returns the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
-	panic("Please implement the InterestRate function")
+	return float32(taux(balance))
 }
 
 // Interest calculates the interest for the provided balance.
 func Interest(balance float64) float64 {
-	panic("Please implement the Interest function")
+	return taux(balance) * balance / 100
 }
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
 func AnnualBalanceUpdate(balance float64) float64 {
-	panic("Please implement the AnnualBalanceUpdate function")
+	return balance + Interest(balance)
 }
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance:
 func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
-	panic("Please implement the YearsBeforeDesiredBalance function")
+	var year = 0
+	for balance < targetBalance {
+		balance = AnnualBalanceUpdate(balance)
+		year++
+	}
+	return year
+}
+
+func taux(solde float64) float64 {
+	switch {
+	case solde < 0:
+		return 3.213
+	case solde < 1000:
+		return 0.5
+	case solde < 5000:
+		return 1.621
+	default:
+		return 2.475
+	}
 }
