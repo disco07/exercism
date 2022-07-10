1c995da9252a6c8778ae542940f4a6fe57340cc2
disco07 <koneenok@outlook.fr>
2022-07-10 11:59:40 +0200

    elon toy finish

--- go/elons-toys/elons_toys.go
+++ go/elons-toys/elons_toys.go
@@ -1,12 +1,32 @@
 package elon
 
-// TODO: define the 'Drive()' method
-func Drive() {
+import "fmt"
 
+// Drive update the number of meters driven and the battery.
+func (c *Car) Drive() {
+	if c.battery > c.batteryDrain {
+		c.distance += c.speed
+		c.battery -= c.batteryDrain
+	}
 }
 
-// TODO: define the 'DisplayDistance() string' method
+// DisplayDistance return the distance.
+func (c Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %v meters", c.distance)
+}
 
-// TODO: define the 'DisplayBattery() string' method
+// DisplayBattery return the battery percentage.
+func (c Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %v%s", c.battery, "%")
+}
 
-// TODO: define the 'CanFinish(trackDistance int) bool' method
+// CanFinish returns true if the car can finish the race; otherwise, return false.
+func (c Car) CanFinish(trackDistance int) bool {
+	var maxDist float64
+	maxDist = (float64(c.speed) / float64(c.batteryDrain)) * float64(c.battery)
+	if maxDist+float64(c.distance) >= float64(trackDistance) {
+		return true
+	} else {
+		return false
+	}
+}
