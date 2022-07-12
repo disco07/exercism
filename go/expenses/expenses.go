556dbc3a0478e852192a7825f6b935410ba49755
disco07 <koneenok@outlook.fr>
2022-07-12 09:36:41 +0200

    expenses

--- go/expenses/expenses.go
+++ go/expenses/expenses.go
@@ -1,5 +1,7 @@
 package expenses
 
+import "errors"
+
 // Record represents an expense record.
 type Record struct {
 	Day      int
@@ -15,26 +17,48 @@ type DaysPeriod struct {
 
 // Filter returns the records for which the predicate function returns true.
 func Filter(in []Record, predicate func(Record) bool) []Record {
-	panic("Please implement the Filter function")
+	var r []Record
+	for _, record := range in {
+		if predicate(record) {
+			r = append(r, record)
+		}
+	}
+	return r
 }
 
 // ByDaysPeriod returns predicate function that returns true when
 // the day of the record is inside the period of day and false otherwise
 func ByDaysPeriod(p DaysPeriod) func(Record) bool {
-	panic("Please implement the ByDaysPeriod function")
+	return func(record Record) bool {
+		if record.Day >= p.To && record.Day <= p.From {
+			return true
+		}
+		return false
+	}
 }
 
 // ByCategory returns predicate function that returns true when
 // the category of the record is the same as the provided category
 // and false otherwise
 func ByCategory(c string) func(Record) bool {
-	panic("Please implement the ByCategory function")
+	return func(record Record) bool {
+		if record.Category == c {
+			return true
+		}
+		return false
+	}
 }
 
 // TotalByPeriod returns total amount of expenses for records
 // inside the period p
 func TotalByPeriod(in []Record, p DaysPeriod) float64 {
-	panic("Please implement the TotalByPeriod function")
+	var count float64
+	for _, record := range in {
+		if ByDaysPeriod(p)(record) {
+			count += record.Amount
+		}
+	}
+	return count
 }
 
 // CategoryExpenses returns total amount of expenses for records
@@ -42,5 +66,15 @@ func TotalByPeriod(in []Record, p DaysPeriod) float64 {
 // An error must be returned only if there are no records in the list that belong
 // to the given category, regardless of period of time.
 func CategoryExpenses(in []Record, p DaysPeriod, c string) (float64, error) {
-	panic("Please implement the CategoryExpenses function")
+	var count float64
+	for _, record := range in {
+		if ByCategory(c)(record) {
+			if ByDaysPeriod(p)(record) {
+				count += record.Amount
+			}
+		} else {
+			return 0, errors.New("false")
+		}
+	}
+	return count, nil
 }
