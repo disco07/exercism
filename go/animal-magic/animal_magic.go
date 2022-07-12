cd2c243797b0159fd9dff200f038fb199ce7f81b
disco07 <koneenok@outlook.fr>
2022-07-12 12:25:24 +0200

    animal magic finish

--- go/animal-magic/animal_magic.go
+++ go/animal-magic/animal_magic.go
@@ -1,21 +1,32 @@
 package chance
 
+import (
+	"math/rand"
+	"time"
+)
+
 // SeedWithTime seeds math/rand with the current computer time
 func SeedWithTime() {
-	panic("Please implement the SeedWithTime function")
+	rand.Intn(int(time.Now().Unix()))
 }
 
 // RollADie returns a random int d with 1 <= d <= 20
 func RollADie() int {
-	panic("Please implement the RollADie function")
+	min := 1
+	max := 20
+	return rand.Intn(max-min) + min
 }
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0
 func GenerateWandEnergy() float64 {
-	panic("Please implement the GenerateWandEnergy function")
+	min := 0
+	max := 12
+	return float64(rand.Intn(max-min) + min)
 }
 
 // ShuffleAnimals returns a slice with all eight animal strings in random order
 func ShuffleAnimals() []string {
-	panic("Please implement the ShuffleAnimals function")
+	a := []string{"ant", "beaver", "cat", "dog", "elephant", "fox", "giraffe", "hedgehog"}
+	rand.Shuffle(len(a), func(i, j int) { a[i], a[j] = a[j], a[i] })
+	return a
 }
