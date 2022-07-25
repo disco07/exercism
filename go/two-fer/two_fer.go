0d6e5ec6af1b008ff68c2845189da1964bd96843
disco07 <koneenok@outlook.fr>
2022-07-25 10:45:05 +0200

    two_fer

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -5,11 +5,12 @@
 // https://golang.org/doc/effective_go.html#commentary
 package twofer
 
+import "fmt"
+
 // ShareWith should have a comment documenting it.
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	if name != "" {
+		return fmt.Sprintf("One for %s, one for me.", name)
+	}
+	return "One for you, one for me."
 }
