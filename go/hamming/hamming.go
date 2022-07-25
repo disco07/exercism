6f379188a8488ac085870c9b0dc2bbf623ce3464
disco07 <koneenok@outlook.fr>
2022-07-25 15:41:31 +0200

    hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,16 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	var count = 0
+	if len(a) != len(b) {
+		return count, errors.New("length of sequences are not equal")
+	}
+	for i, r := range []rune(a) {
+		if r != []rune(b)[i] {
+			count++
+		}
+	}
+	return count, nil
 }
