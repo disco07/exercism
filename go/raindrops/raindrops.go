31016fa052012afd12802fd658c08f0d8d4bef62
disco07 <koneenok@outlook.fr>
2022-07-26 22:28:11 +0200

    raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,5 +1,20 @@
 package raindrops
 
+import "fmt"
+
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	var s = ""
+	if number%3 == 0 {
+		s += "Pling"
+	}
+	if number%5 == 0 {
+		s += "Plang"
+	}
+	if number%7 == 0 {
+		s += "Plong"
+	}
+	if s == "" {
+		s = fmt.Sprintf("%v", number)
+	}
+	return s
 }
