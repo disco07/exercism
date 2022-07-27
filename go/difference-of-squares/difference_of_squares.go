ce63ced944c730d04fbfcc274f92ee79df76cd43
disco07 <koneenok@outlook.fr>
2022-07-27 09:18:33 +0200

    difference_of_square

---
+++ go/difference-of-squares/difference_of_squares.go
@@ -0,0 +1,21 @@
+package diffsquares
+
+import "math"
+
+func SquareOfSum(n int) int {
+	i := 1
+	var count int
+	for i <= 5 {
+		count += i
+		i++
+	}
+	math.Sqrt()
+}
+
+func SumOfSquares(n int) int {
+	panic("Please implement the SumOfSquares function")
+}
+
+func Difference(n int) int {
+	panic("Please implement the Difference function")
+}
