45f8c6ff9d8620f370c048956638be374c8d7da8
disco07 <koneenok@outlook.fr>
2022-07-27 09:29:54 +0200

    difference_of_square

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,21 +1,25 @@
 package diffsquares
 
-import "math"
-
 func SquareOfSum(n int) int {
 	i := 1
 	var count int
-	for i <= 5 {
+	for i <= n {
 		count += i
 		i++
 	}
-	math.Sqrt()
+	return count * count
 }
 
 func SumOfSquares(n int) int {
-	panic("Please implement the SumOfSquares function")
+	i := 1
+	var count int
+	for i <= n {
+		count += i * i
+		i++
+	}
+	return count
 }
 
 func Difference(n int) int {
-	panic("Please implement the Difference function")
+	return SquareOfSum(n) - SumOfSquares(n)
 }
