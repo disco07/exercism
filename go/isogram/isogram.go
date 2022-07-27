b85ea9882b27bd8295e0e4f6113492c7585934f6
disco07 <koneenok@outlook.fr>
2022-07-27 09:05:35 +0200

    isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,5 +1,19 @@
 package isogram
 
+import "strings"
+
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	var count int
+	for _, w := range []rune(word) {
+		count = 0
+		for _, w2 := range []rune(word) {
+			if strings.ToLower(string(w)) == strings.ToLower(string(w2)) && string(w) != "-" && string(w) != " " {
+				count++
+			}
+			if count > 1 {
+				return false
+			}
+		}
+	}
+	return true
 }
