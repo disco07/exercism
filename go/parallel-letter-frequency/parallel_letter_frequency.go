37764e28f7566b45461482d57111c8cb95e4acda
disco07 <koneenok@outlook.fr>
2022-07-27 21:17:03 +0200

    parallel_letter_frequency

---
+++ go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -0,0 +1,20 @@
+package letter
+
+// FreqMap records the frequency of each rune in a given text.
+type FreqMap map[rune]int
+
+// Frequency counts the frequency of each rune in a given text and returns this
+// data as a FreqMap.
+func Frequency(s string) FreqMap {
+	m := FreqMap{}
+	for _, r := range s {
+		m[r]++
+	}
+	return m
+}
+
+// ConcurrentFrequency counts the frequency of each rune in the given strings,
+// by making use of concurrency.
+func ConcurrentFrequency(l []string) FreqMap {
+	panic("Implement the ConcurrentFrequency function")
+}
